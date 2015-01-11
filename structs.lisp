3fe19e4159dec389cc7bbccef00b00e5ec8db946
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2015-01-11 17:37:13 +0100

    initialization of board mostly complete

--- structs.lisp
+++ structs.lisp
@@ -1,35 +1,18 @@
 (in-package :org.kjerkreit.sudoku-solver)
 
 (defstruct cell
-  (row nil)
-  (col nil)
-  (box nil)
-  (value 0)
-  (domain 512))
+  (row 0 :type (integer 0 8))
+  (col 0 :type (integer 0 8))
+  (box 0 :type (integer 0 8))
+  (value 0 :type (integer 0 9))
+  (domain '(1 2 3 4 5 6 7 8 9) :type list))
 
 (defstruct element
-  (found-vals 0)
-  (cells (make-array 9
-                     :element-type 'fixnum
-                     :adjustable nil
-                     :fill-pointer nil)))
-
-(defun cell-vector ()
-
-  (make-array 81
-              :element-type 'cell
-              :adjustable nil
-              :fill-pointer nil))
-
-(defun elt-vector ()
-
-  (make-array 9
-              :element-type 'element
-              :adjustable nil
-              :fill-pointer nil))
+  (found-vals '() :type list)
+  (cells (make-array 9)))
 
 (defstruct board
-  (rows (elt-vector))
-  (cols (elt-vector))
-  (boxes (elt-vector))
-  (cells (cell-vector)))
+  (rows (make-array 9 :element-type 'element))
+  (cols (make-array 9 :element-type 'element))
+  (boxes (make-array 9 :element-type 'element))
+  (cells (make-array 81 :element-type 'cell)))
