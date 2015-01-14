89d79eb877ca1b81f2e0de3c819c270e537f8e37
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2015-01-14 10:03:15 +0100

    ugly pseudo accessors defined

--- support.lisp
+++ support.lisp
@@ -11,6 +11,7 @@
              (format s "~a" item))))))
 
 (defun string-to-ints (string)
+  "Converts a string to a list of single digit ints."
 
   (log:trace "entry")
   (loop for c across string
@@ -18,12 +19,30 @@
        finally (return (mapcar #'parse-integer ints))))
 
 (defun read-puzzle (fname)
+  "Read a sudoku puzzle from disk."
+
   (log:trace "entry")
   (with-open-file (s fname :direction :input)
            (loop for line = (read-line s nil nil)
               until (eq line nil)
               collect line into lines
               finally (return (string-to-ints (concat-strings lines))))))
+
+;; needs to be turned into a macro
+(defun get-row (board index)
+  "Returns the actual row."
+
+  (aref (board-rows board) index))
+
+(defun get-col (board index)
+  "Returns the actual column."
+
+  (aref (board-cols board) index))
+
+(defun get-box (board index)
+  "Returns the actual box."
+
+  (aref (board-boxes board) index))
 
 (defmacro aset (array subscript value)
   `(setf (aref ,array , subscript) ,value))
