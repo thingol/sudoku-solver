830123ab600cede487bfdd227b30d2df1cd36a65
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2015-01-26 21:04:06 +0100

    makes the reports a wee bit easier to read

--- timing.sh
+++ timing.sh
@@ -4,7 +4,8 @@ for class in $(echo "EASY MEDIUM HARD FIENDISH" | cut -d' ' -f1- - ); do
     echo -n "$class"
     for i in $(seq 0 9); do
         echo -n " $i"
-        sbcl --no-inform --load qnd-load.lisp  test/${class}.sudoku_puzzle 45000 >> test/${class}.report
+        echo "==================================================================================" >> test/${class}.report
+        sbcl --noinform --dynamic-space-size 4096 --load qnd-load.lisp  test/${class}.sudoku_puzzle 1 >> test/${class}.report
     done
     echo ""
 done
